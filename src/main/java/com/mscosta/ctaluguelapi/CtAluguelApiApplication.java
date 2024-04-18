448741476b94de2e576aa2820763b6b7b5563291
Manoel Costa <costamanoelsc@gmail.com>
2024-04-18 00:53:47 -0300

    Initial Commit

---
+++ src/main/java/com/mscosta/ctaluguelapi/CtAluguelApiApplication.java
@@ -0,0 +1,13 @@
+package com.mscosta.ctaluguelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtAluguelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtAluguelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mscosta/ctaluguelapi/CtAluguelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mscosta.ctaluguelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtAluguelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
